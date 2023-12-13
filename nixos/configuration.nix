5a79bc9a810537eea9ff505855e007a64f6f055e
yuwki0131 <yuwkixxx@gmail.com>
2023-12-14 01:09:07 +0900

    update configuration.nix for networking

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -15,14 +15,14 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  # networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Asia/Tokyo";
@@ -132,10 +132,16 @@
       vim
       emacs
 
-      ## trials ..................
       # utils
       htop
       docker
+
+      # hypr
+      hyprpaper
+      eww-wayland
+
+      ## trials ..................
+
     ];
   };
 
