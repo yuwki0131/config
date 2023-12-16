b144ae14526ec538f164a54c59d6a4aadf65a4a8
yuwki0131 <yuwkixxx@gmail.com>
2023-12-16 16:17:03 +0900

    switch wallpaper by workspace

---
+++ hypr/README.md
@@ -0,0 +1,15 @@
+# Hyperland configuration
+
+## Workspace
+
+### Workspace Theme
+* workspace1: nature
+* workspace2: cloud
+* workspace3: darknight
+* workspace4: volcano
+* workspace5: sunrise
+* workspace6: earth
+* workspace7: windows
+* workspace8: red astro
+* workspace9: cat
+
---
+++ init/init-wallpapers.sh
@@ -0,0 +1,16 @@
+cd ~/myconfig/wallpaper
+
+# workspace 123
+curl -o nature.jpg https://r4.wallpaperflare.com/wallpaper/722/208/39/5bd343d742dcd-wallpaper-48e6ad48d0201c8880cc31be0872e47a.jpg
+curl -o cloud.jpg https://r4.wallpaperflare.com/wallpaper/553/516/984/hawaii-sunset-ocean-beach-waves-clouds-4k-ultra-hd-wallpaper-3840%C3%972160-wallpaper-3ab8c8db557cb3e29fea5078dac8aad2.jpg
+curl -o darknight.jpg https://r4.wallpaperflare.com/wallpaper/988/455/665/night-futuristic-city-cyberpunk-cyber-wallpaper-58f6fd7880b01c58f0ecb1ae7832d49a.jpg
+
+# workspace 456
+curl -o volcano.jpg https://www.hdwallpapers.in/download/volcano_4k_hd-HD.jpg
+curl -o sunrise.jpg https://r4.wallpaperflare.com/wallpaper/553/516/984/hawaii-sunset-ocean-beach-waves-clouds-4k-ultra-hd-wallpaper-3840%C3%972160-wallpaper-3ab8c8db557cb3e29fea5078dac8aad2.jpg
+curl -o earth.jpg https://r4.wallpaperflare.com/wallpaper/12/1008/45/earth-4k-best-desktop-download-wallpaper-b876cd2860b07c48b03ca18e6852943a.jpg
+
+# wokspace 789
+curl -o windows.jpg https://r4.wallpaperflare.com/wallpaper/974/565/254/windows-11-windows-10-minimalism-hd-wallpaper-c876bde870303c5820cce16ed8a244ca.jpg
+curl -o redastro.jpg https://i.redd.it/national-park-4k-3840x2160-by-a-i-v0-g4crddfnmt9a1.jpg?s=09b1d04542d7641ca8f15e7d41a4c8786926be97
+curl -o cat.jpg https://i.redd.it/djpdae3zg5z41.jpg
