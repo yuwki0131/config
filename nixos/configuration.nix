f52730bdfc06c68bbc7bedda2b1be362207006cf
yuwki0131 <yuwkixxx@gmail.com>
2023-12-16 13:51:52 +0900

    update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      /etc/nixos/hardware-configuration.nix
     ];
 
   # Bootloader.
@@ -15,14 +15,14 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  # networking.networkmanager.enable = true;
+  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Asia/Tokyo";
@@ -122,10 +122,13 @@
 
       # tools
       git
+      fast-cli
+      googler
 
       # terminals
       terminator
       guake
+      hyper
 
       # editor
       vscode
