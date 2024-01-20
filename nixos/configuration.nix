2805ad638f20b343dfa5224c553dc15dac68a6c6
yuwkixxx@gmail.com <yuwki0131>
2024-01-20 18:35:50 +0900

    fix audio config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -93,12 +93,12 @@
 
   # Enable sound with pipewire.
   sound.enable = true;
-  hardware.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    enable = false;
+    alsa.enable = false;
+    alsa.support32Bit = false;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
