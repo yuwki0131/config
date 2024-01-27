2fd6c76fed3cc414b312c0c46d2704519c99750d
yuwkixxx@gmail.com <yuwki0131>
2024-01-27 17:04:16 +0900

    add init-wallpapers for eDP-1

---
+++ init/init-wallpapers-2880x1800.sh
@@ -0,0 +1,16 @@
+cd ~/myconfig/wallpaper
+
+# workspace 123
+curl -o nature.jpg https://r4.wallpaperflare.com/wallpaper/377/444/45/summer-landscape-lake-flowers-green-nature-blue-sky-clouds-beautiful-hd-wallpaper-2880%C3%971800-wallpaper-0f0cb22f9df91588076d3234f443e010.jpg
+curl -o cloud.jpg https://wallpapersmug.com/download/2880x1800/a801aa/sky-clouds-original-anime.jpg
+curl -o darknight.jpg https://wallpapersmug.com/download/2880x1800/d3f2e9/dark-city-in-night-cityscape.jpg
+
+# workspace 456
+curl -o volcano.jpg https://r4.wallpaperflare.com/wallpaper/328/708/873/5bd3ad1fba085-wallpaper-5f5c923fcdb9759857fda2e464037070.jpg
+curl -o sunrise.jpg https://wallpaperfx.com/view_image/gorgeous-winter-sunrise-2880x1800-wallpaper-15150.jpg
+curl -o earth.jpg https://images.hdqwalls.com/download/earth-space-hd-2880x1800.jpg
+
+# wokspace 789
+curl -o windows.jpg https://images.wallpapersden.com/image/download/windows-11-4k-layers_bWtmbG2UmZqaraWkpJRobWlmrWdpZWU.jpg
+curl -o redastro.jpg https://images.wallpapersden.com/image/download/cool-red-mountains-4k_bG1mamyUmZqaraWkpJRnbW1lrWZtZWU.jpg
+curl -o cat.jpg https://wallpapersmug.com/download/2880x1800/da7e51/cute-feline-cate.jpg
