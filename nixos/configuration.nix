893c31e0e608818050f8a308b1047af43b2de3ef
yuwki0131 <yuwkixxx@gmail.com>
2024-07-21 23:39:30 +0900

    change desktop and display manager from plasma to cinnamon

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -55,9 +55,9 @@
   # hyprland
   programs.hyprland.enable=true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  # Enable the Cinnamon Desktop Environment.
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.cinnamon.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
