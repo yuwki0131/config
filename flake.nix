e897b93728126efaaf547f81ea4b6f3861f92900
yuwki0131 <yuwkixxx@gmail.com>
2025-06-08 11:07:53 +0900

    flake base replace

--- flake.nix
+++ flake.nix
@@ -6,6 +6,10 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
+      hardwareConfig =
+        if builtins.pathExists ./.github/workflows/testfiles/hardware-configuration.nix
+        then ./testfiles/hardware-configuration.nix
+        else /etc/nixos/hardware-configuration.nix;
     in {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         inherit system;
