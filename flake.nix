336f8726bcc77703f97d1c0f35fffa4a497f60df
yuwki0131 <yuwkixxx@gmail.com>
2025-06-08 11:15:32 +0900

    remove hardware configuration around

--- flake.nix
+++ flake.nix
@@ -6,10 +6,6 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      hardwareConfig =
-        if builtins.pathExists ./.github/workflows/testfiles/hardware-configuration.nix
-        then ./testfiles/hardware-configuration.nix
-        else /etc/nixos/hardware-configuration.nix;
     in {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         inherit system;
